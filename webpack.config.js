d11212a7e69b4eb61b43dd431f1cae27959f1e1e
qwartzuiop <22412777+qwartzuiop@users.noreply.github.com>
2021-12-14 19:10:44 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/main.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[id].[chunkhash].js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|webp)$/,
+                include: path.resolve(__dirname, 'src'),
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[hash][ext]'
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+            inject: true,
+            chunks: ['main'],
+            filename: 'index.html',
+        }),
+    ],
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    devServer: {
+        hot: true,
+        // https: true,
+    },
+};
